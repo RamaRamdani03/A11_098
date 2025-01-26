ae1f3dc6a22862f6786d0aded6a64cf266caca4a
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 08:28:24 +0700

    Menambahkan objek DestinasiHome untuk navigasi, dengan mendefinisikan rute "home" dan judul yang terkait.

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiHome: AlamatNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
