71e30b0f5a159498508e0959c08f760f44849c45
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 08:34:49 +0700

    Menambahkan komposable HomeNavbar dengan aksi untuk Anggota, Buku, Peminjaman, dan Pengembalian, mendukung kustomisasi modifier.

--- app/src/main/java/com/example/perpustakaan/ui/view/HomeView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/HomeView.kt
@@ -1,8 +1,21 @@
 package com.example.perpustakaan.ui.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
 object DestinasiHome: AlamatNavigasi {
     override val route = "home"
     override val titleRes = "Home"
+}
+
+@Composable
+fun HomeNavbar(
+    modifier: Modifier = Modifier,
+    onAnggota: () -> Unit = {},
+    onBuku: () -> Unit = {},
+    onPeminjaman: () -> Unit = {},
+    onPengembalian: () -> Unit = {},
+) {
+
 }
