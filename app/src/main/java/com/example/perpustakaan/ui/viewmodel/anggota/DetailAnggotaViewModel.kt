a4dec66cff0bb409dca35ca27f7716ab6c9cd50e
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-27 01:37:17 +0700

    Menambahkan DetailAnggotaViewModel untuk mengambil dan mengelola detail anggota dengan status loading dan error

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/DetailAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/DetailAnggotaViewModel.kt
@@ -1,6 +1,37 @@
 package com.example.perpustakaan.ui.viewmodel.anggota
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.model.Anggota
+import com.example.perpustakaan.data.repository.AnggotaRepository
+import kotlinx.coroutines.launch
+
+class DetailAnggotaViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
+
+
+
+    var uiState by mutableStateOf(DetailAnggotaUiState())
+        private set
+
+    fun fetchDetailAnggota(id_anggota: Int) {
+        viewModelScope.launch {
+            uiState = DetailAnggotaUiState(isLoading = true)
+            try {
+                val anggota = anggotaRepository.getAnggotaById(id_anggota)
+                uiState = DetailAnggotaUiState(detailUiEvent = anggota.data.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailAnggotaUiState(
+                    isError = true,
+                    errrorMessage = "Failed to fetch anggota details: ${e.message}"
+                )
+            }
+        }
+    }
+}
 
 data class DetailAnggotaUiState(
     val detailUiEvent: InsertAnggotaUiEvent = InsertAnggotaUiEvent(),
