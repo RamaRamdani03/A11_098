9dd74ae1885658d0ce3cb09e520e996df33bf3a5
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-27 02:06:54 +0700

    DetailRow composable untuk menampilkan label dan nilai dalam format baris

--- app/src/main/java/com/example/perpustakaan/ui/view/anggota/DetailAnggotaView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/anggota/DetailAnggotaView.kt
@@ -1,5 +1,13 @@
 package com.example.perpustakaan.ui.view.anggota
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
 object DestinasiDetailAnggota : AlamatNavigasi {
@@ -7,4 +15,24 @@ object DestinasiDetailAnggota : AlamatNavigasi {
     override val titleRes = "Detail Anggota"
     const val id_anggota = "id_anggota"
     val routeWithArgs = "$route/{$id_anggota}"
+}
+
+@Composable
+fun DetailRow(label: String, value: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = ": $value",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(2f)
+        )
+    }
 }
