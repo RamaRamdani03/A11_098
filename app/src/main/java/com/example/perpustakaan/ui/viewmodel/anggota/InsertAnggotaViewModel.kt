e8fe31c50d1f5aa2b200052c2778159daf6cf4b1
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 23:18:47 +0700

    fungsi untuk memperbarui dan menyimpan data anggota dengan validasi input

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
@@ -1,11 +1,45 @@
 package com.example.perpustakaan.ui.viewmodel.anggota
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.model.Anggota
 import com.example.perpustakaan.data.repository.AnggotaRepository
+import kotlinx.coroutines.launch
 
 class InsertAnggotaViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
 
+    var uiState by mutableStateOf(InsertAnggotaUiState())
+        private set
+
+    fun updateInsertAnggotaState(insertUiEvent: InsertAnggotaUiEvent) {
+        uiState = InsertAnggotaUiState(
+            insertUiEvent = insertUiEvent
+        )
+    }
+
+    fun insertAnggota() {
+        val event = uiState.insertUiEvent
+
+        // Validasi input sebelum insert
+        if (event.nama.isBlank() || event.email.isBlank() || event.nomor_telepon.isBlank()) {
+            println("Gagal menyimpan: Semua data harus diisi.")
+            return
+        }
+
+        viewModelScope.launch {
+            try {
+                println("Menyimpan anggota: $event")
+                anggotaRepository.insertAnggota(event.toAnggota())
+                println("Anggota berhasil disimpan.")
+            } catch (e: Exception) {
+                println("Error saat menyimpan anggota: ${e.message}")
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertAnggotaUiState(
