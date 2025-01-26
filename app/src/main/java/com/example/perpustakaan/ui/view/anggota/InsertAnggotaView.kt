dc525766baa181af0a1990c258dd8d90b7e07d5a
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 23:39:03 +0700

    Menambahkan objek DestinasiInsertAnggota untuk mengimplementasikan AlamatNavigasi dengan route dan titleRes

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/anggota/InsertAnggotaView.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.anggota
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiInsertAnggota: AlamatNavigasi {
+    override val route: String = "item_entryanggota"
+    override val titleRes = "Entry Anggota"
+}
