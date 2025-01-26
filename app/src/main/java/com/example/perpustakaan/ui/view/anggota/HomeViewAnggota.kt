911572ba00606b29d262fe91800ccf46c931f6c8
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 22:25:19 +0700

    Menambahkan objek DestinasiHomeAnggota untuk mengimplementasikan AlamatNavigasi dengan properti route dan titleRes

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/anggota/HomeViewAnggota.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.anggota
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiHomeAnggota : AlamatNavigasi {
+    override val route = "homeAnggota"
+    override val titleRes = "Anggota Perpustakaan"
+}
