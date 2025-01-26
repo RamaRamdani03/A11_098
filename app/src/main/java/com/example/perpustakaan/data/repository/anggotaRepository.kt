8d90bec51534d20cbaa8184bb07021e89e9cbd04
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 07:17:11 +0700

    implementasi NetworkAnggotaRepository dengan fungsi CRUD menggunakan AnggotaService

--- app/src/main/java/com/example/perpustakaan/data/repository/anggotaRepository.kt
+++ app/src/main/java/com/example/perpustakaan/data/repository/anggotaRepository.kt
@@ -2,6 +2,9 @@ package com.example.perpustakaan.data.repository
 
 import com.example.perpustakaan.data.model.Anggota
 import com.example.perpustakaan.data.model.AnggotaResponseDetail
+import com.example.perpustakaan.data.service_api.AnggotaService
+import kotlinx.coroutines.flow.Flow
+import java.io.IOException
 
 interface AnggotaRepository {
     suspend fun getAnggota(): List<Anggota>
@@ -9,4 +12,43 @@ interface AnggotaRepository {
     suspend fun updateAnggota(idAnggota: Int, anggota: Anggota)
     suspend fun deleteAnggota(idAnggota: Int)
     suspend fun getAnggotaById(idAnggota: Int): AnggotaResponseDetail
+}
+
+class NetworkAnggotaRepository(
+    private val anggotaApiService: AnggotaService
+) : AnggotaRepository {
+
+    // Menambahkan data anggota
+    override suspend fun insertAnggota(anggota: Anggota) {
+        anggotaApiService.insertAnggota(anggota)
+    }
+
+    // Memperbarui data anggota
+    override suspend fun updateAnggota(idAnggota: Int, anggota: Anggota) {
+        anggotaApiService.updateAnggota(idAnggota, anggota)
+    }
+
+    // Menghapus data anggota berdasarkan ID anggota
+    override suspend fun deleteAnggota(idAnggota: Int) {
+        try {
+            val response = anggotaApiService.deleteAnggota(idAnggota)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete anggota. HTTP status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    // Mendapatkan seluruh data anggota
+    override suspend fun getAnggota(): List<Anggota> = anggotaApiService.getAnggota().data
+
+    // Mendapatkan detail anggota berdasarkan ID
+    override suspend fun getAnggotaById(idAnggota: Int): AnggotaResponseDetail {
+        return anggotaApiService.getAnggotaById(idAnggota)
+    }
 }
