6767fa46e07ba2974f65015675f94e076cb0b00d
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-26 22:50:26 +0700

    Menambahkan data class InsertAnggotaUiState dan InsertAnggotaUiEvent untuk mengelola state UI dan event dalam proses penambahan anggota

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.perpustakaan.ui.viewmodel.anggota
 
+import androidx.lifecycle.ViewModel
+import com.example.perpustakaan.data.model.Anggota
+import com.example.perpustakaan.data.repository.AnggotaRepository
+
+class InsertAnggotaViewModel(private val anggotaRepository: AnggotaRepository) : ViewModel() {
+
+}
+
 data class InsertAnggotaUiState(
     val insertUiEvent: InsertAnggotaUiEvent = InsertAnggotaUiEvent(),
 )
@@ -9,4 +17,22 @@ data class InsertAnggotaUiEvent(
     val nama: String = "",
     val email: String = "",
     val nomor_telepon: String = ""
+)
+
+fun InsertAnggotaUiEvent.toAnggota(): Anggota = Anggota(
+    id_anggota = 0,
+    nama = nama,
+    email = email,
+    nomor_telepon = nomor_telepon
+)
+
+fun Anggota.toUiStateAnggota(): InsertAnggotaUiState = InsertAnggotaUiState(
+    insertUiEvent = toInsertAnggotaUiEvent()
+)
+
+fun Anggota.toInsertAnggotaUiEvent(): InsertAnggotaUiEvent = InsertAnggotaUiEvent(
+    id_anggota = 0,
+    nama = nama,
+    email = email,
+    nomor_telepon = nomor_telepon
 )
