7836b6ebbacd0c73ab1590f90bb0fe3f5b640282
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 05:55:40 +0700

    mengimplementasikan navigasi untuk HomeNavbar

--- app/src/main/java/com/example/perpustakaan/ui/navigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/perpustakaan/ui/navigasi/PengelolaanHalaman.kt
@@ -5,7 +5,15 @@ import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.perpustakaan.ui.view.DestinasiHome
+import com.example.perpustakaan.ui.view.HomeNavbar
+import com.example.perpustakaan.ui.view.anggota.DestinasiHomeAnggota
+import com.example.perpustakaan.ui.view.buku.DestinasiHomeBuku
+import com.example.perpustakaan.ui.view.peminjaman.DestinasiHomePeminjaman
+import com.example.perpustakaan.ui.view.pengembalian.DestinasiHomePengembalian
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -13,5 +21,19 @@ fun PengelolaanHalaman (
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
-
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route
+    ) {
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeNavbar(
+                onBuku = { navController.navigate(DestinasiHomeBuku.route) },
+                onAnggota = { navController.navigate(DestinasiHomeAnggota.route) },
+                onPeminjaman = { navController.navigate(DestinasiHomePeminjaman.route) },
+                onPengembalian = { navController.navigate(DestinasiHomePengembalian.route) }
+            )
+        }
+    }
 }
