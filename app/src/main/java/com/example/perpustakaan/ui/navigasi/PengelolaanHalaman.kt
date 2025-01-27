dde81b994704e3fa839802a47cc17c793f99e2c6
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 05:53:14 +0700

    Menambahkan pengaturan awal untuk Composable PengelolaanHalaman dengan NavHostController

---
+++ app/src/main/java/com/example/perpustakaan/ui/navigasi/PengelolaanHalaman.kt
@@ -0,0 +1,17 @@
+package com.example.perpustakaan.ui.navigasi
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun PengelolaanHalaman (
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+
+}
