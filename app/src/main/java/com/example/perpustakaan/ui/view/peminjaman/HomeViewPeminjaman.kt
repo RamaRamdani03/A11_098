1c4141342c05a17d4c5a2e5d8069390b8be88025
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 21:59:58 +0700

    Menambahkan komponen PeminjamanCard untuk menampilkan detail peminjaman dengan opsi edit dan delete

--- app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/HomeViewPeminjaman.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/HomeViewPeminjaman.kt
@@ -2,18 +2,150 @@ package com.example.perpustakaan.ui.view.peminjaman
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.R
+import com.example.perpustakaan.data.model.Peminjaman
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
 object DestinasiHomePeminjaman : AlamatNavigasi {
     override val route = "homePeminjaman"
     override val titleRes = "Data Peminjaman"
+}
+
+@Composable
+fun PeminjamanCard(
+    peminjaman: Peminjaman,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Peminjaman) -> Unit = {},
+    onEditPeminjaman: (Int) -> Unit
+) {
+    val showDialog = remember { mutableStateOf(false) }
+
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.AccountBox, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "ID Peminjaman: ${peminjaman.id_peminjaman}",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Filled.Face,
+                    contentDescription = "Nama Anggota"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Nama Anggota: ${peminjaman.nama}")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(painter = painterResource(R.drawable.bukuuuuuu),
+                    contentDescription = "Judul Buku",
+                    modifier = Modifier.size(30.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "Judul Buku: ${peminjaman.judul}")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Default.DateRange,
+                    contentDescription = "Tanggal Peminjaman"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Tanggal Peminjaman: ${peminjaman.tanggal_peminjaman}")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Default.DateRange,
+                    contentDescription = "Tanggal Pengembalian"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Tanggal Pengembalian: ${peminjaman.tanggal_pengembalian}")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "Status"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Status: ${peminjaman.status}")
+            }
+
+                Spacer(Modifier.weight(1f))
+
+            // Tombol Delete
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = { showDialog.value = true }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Peminjaman"
+                    )
+                }
+
+                // Edit
+                IconButton(onClick = { onEditPeminjaman(peminjaman.id_peminjaman) }) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit Peminjaman"
+                    )
+                }
+            }
+        }
+    }
+
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = { Text(text = "Konfirmasi Hapus") },
+            text = { Text("Apakah Anda yakin ingin menghapus data peminjaman ini?") },
+            confirmButton = {
+                TextButton(onClick = {
+                    onDeleteClick(peminjaman)
+                    showDialog.value = false
+                }) {
+                    Text("Ya")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog.value = false }) {
+                    Text("Tidak")
+                }
+            }
+        )
+    }
 }
 
 @Composable
