e7ca9268830b369d8eab1faba5361d34e2ffcbd7
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 22:45:39 +0700

    Menambahkan objek DestinasiHomePengembalian untuk navigasi ke layar Data Pengembalian

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/pengembalian/HomeViewPengembalian.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.pengembalian
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiHomePengembalian : AlamatNavigasi {
+    override val route = "homePengembalian"
+    override val titleRes = "Data Pengembalian"
+}
