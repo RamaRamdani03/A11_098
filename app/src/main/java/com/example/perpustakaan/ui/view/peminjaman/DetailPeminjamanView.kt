0e15788d049542e297148f12d3e8e74c05d593f5
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 22:10:25 +0700

    Menambahkan komponen DetailRow untuk menampilkan label dan nilai secara berurutan dalam tampilan baris

--- app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/DetailPeminjamanView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/DetailPeminjamanView.kt
@@ -1,5 +1,13 @@
 package com.example.perpustakaan.ui.view.peminjaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
 object DestinasiDetailPeminjaman : AlamatNavigasi {
@@ -7,4 +15,24 @@ object DestinasiDetailPeminjaman : AlamatNavigasi {
     override val titleRes = "Detail Peminjaman"
     const val id_peminjaman = "id_peminjaman"
     val routeWithArgs = "$route/{$id_peminjaman}"
+}
+
+@Composable
+fun DetailRow(label: String, value: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = ": $value",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(2f)
+        )
+    }
 }
