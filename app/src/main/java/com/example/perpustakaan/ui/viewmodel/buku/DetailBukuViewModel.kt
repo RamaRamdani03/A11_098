9216a8da21012dacdc06848b80d2b5be1a71435c
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 18:45:59 +0700

    Menambahkan ViewModel untuk mengelola state dan mengambil detail buku berdasarkan ID

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/buku/DetailBukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/buku/DetailBukuViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.perpustakaan.ui.viewmodel.buku
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.model.Buku
+import com.example.perpustakaan.data.repository.BukuRepository
+import kotlinx.coroutines.launch
+
+class DetailBukuViewModel(private val bukuRepository: BukuRepository): ViewModel() {
+
+    var uiState by mutableStateOf(DetailBukuUiState())
+        private set
+
+    fun fetchDetailBuku(id_buku: Int) {
+        viewModelScope.launch {
+            uiState = DetailBukuUiState(isLoading = true)
+            try {
+                val buku = bukuRepository.getBukuById(id_buku)
+                uiState = DetailBukuUiState(detailUiEvent = buku.data.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailBukuUiState(
+                    isError = true,
+                    errorMessage = "Failed to fetch buku details: ${e.message}"
+                )
+            }
+        }
+    }
+}
 
 data class DetailBukuUiState(
     val detailUiEvent: InsertBukuUiEvent = InsertBukuUiEvent(),
