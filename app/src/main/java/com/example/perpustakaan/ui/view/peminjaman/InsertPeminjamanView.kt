b06bc5e44327bfe914f7b80430be2b5fffd1c020
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 22:15:34 +0700

    Menambahkan objek destinasi navigasi untuk halaman Entry Peminjaman

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/InsertPeminjamanView.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.peminjaman
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiInsertPeminjaman : AlamatNavigasi {
+    override val route: String = "item_entrypeminjaman"
+    override val titleRes = "Entry Peminjaman"
+}
