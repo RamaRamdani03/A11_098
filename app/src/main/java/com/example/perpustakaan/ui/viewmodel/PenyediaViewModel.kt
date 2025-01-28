48b13d42d4362eac440f3ce69d3a057616912aa1
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 19:03:54 +0700

    Menambahkan inisialisasi ViewModel untuk Buku, Insert, Update, dan Detail

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/PenyediaViewModel.kt
@@ -10,6 +10,10 @@ import com.example.perpustakaan.ui.viewmodel.anggota.DetailAnggotaViewModel
 import com.example.perpustakaan.ui.viewmodel.anggota.HomeViewModelAnggota
 import com.example.perpustakaan.ui.viewmodel.anggota.InsertAnggotaViewModel
 import com.example.perpustakaan.ui.viewmodel.anggota.UpdateAnggotaViewModel
+import com.example.perpustakaan.ui.viewmodel.buku.DetailBukuViewModel
+import com.example.perpustakaan.ui.viewmodel.buku.HomeViewModelBuku
+import com.example.perpustakaan.ui.viewmodel.buku.InsertBukuViewModel
+import com.example.perpustakaan.ui.viewmodel.buku.UpdateBukuViewModel
 
 object PenyediaViewModel {
 
@@ -19,6 +23,12 @@ object PenyediaViewModel {
         initializer { InsertAnggotaViewModel(perpustakaanAPP().container.anggotaRepository) }
         initializer { UpdateAnggotaViewModel(createSavedStateHandle(),perpustakaanAPP().container.anggotaRepository) }
         initializer { DetailAnggotaViewModel(perpustakaanAPP().container.anggotaRepository) }
+
+        //Buku
+        initializer { HomeViewModelBuku(perpustakaanAPP().container.bukuRepository) }
+        initializer { InsertBukuViewModel(perpustakaanAPP().container.bukuRepository) }
+        initializer { UpdateBukuViewModel(createSavedStateHandle(),perpustakaanAPP().container.bukuRepository) }
+        initializer { DetailBukuViewModel(perpustakaanAPP().container.bukuRepository) }
     }
 }
 
