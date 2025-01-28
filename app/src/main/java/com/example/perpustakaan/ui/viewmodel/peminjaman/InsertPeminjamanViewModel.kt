3bab1efa96d132f34e6992350091b3b77abfacd0
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 21:36:18 +0700

    Mengimplementasikan logika untuk memvalidasi dan menyimpan data peminjaman buku

--- app/src/main/java/com/example/perpustakaan/ui/viewmodel/peminjaman/InsertPeminjamanViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/viewmodel/peminjaman/InsertPeminjamanViewModel.kt
@@ -1,6 +1,48 @@
 package com.example.perpustakaan.ui.viewmodel.peminjaman
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perpustakaan.data.model.Peminjaman
+import com.example.perpustakaan.data.repository.PeminjamanRepository
+import kotlinx.coroutines.launch
+
+class InsertPeminjamanViewModel(
+    private val peminjamanRepository: PeminjamanRepository
+) : ViewModel() {
+
+    var uiState by mutableStateOf(InsertPeminjamanUiState())
+        private set
+
+    fun updateInsertPeminjamanState(insertUiEvent: InsertPeminjamanUiEvent) {
+        uiState = InsertPeminjamanUiState(
+            insertUiEvent = insertUiEvent
+        )
+    }
+
+    fun insertPeminjaman() {
+        val event = uiState.insertUiEvent
+
+        // Validasi input
+        if (event.id_buku <= 0 || event.id_anggota <= 0 || event.tanggal_peminjaman.isBlank() || event.tanggal_pengembalian.isBlank()) {
+            println("Gagal menyimpan: Semua data harus diisi.")
+            return
+        }
+
+        viewModelScope.launch {
+            try {
+                println("Menyimpan peminjaman: $event")
+                peminjamanRepository.insertPeminjaman(event.toPeminjaman())
+                println("Peminjaman berhasil disimpan.")
+            } catch (e: Exception) {
+                println("Error saat menyimpan peminjaman: ${e.message}")
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertPeminjamanUiState(
     val insertUiEvent: InsertPeminjamanUiEvent = InsertPeminjamanUiEvent(),
