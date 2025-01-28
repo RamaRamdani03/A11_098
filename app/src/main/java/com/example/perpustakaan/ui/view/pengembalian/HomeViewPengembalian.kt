f87165780bd17529b9abe8aac55440f580ce4be5
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 22:49:29 +0700

    Menambahkan komponen OnLoading untuk menampilkan animasi pemuatan dengan ikon

--- app/src/main/java/com/example/perpustakaan/ui/view/pengembalian/HomeViewPengembalian.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/pengembalian/HomeViewPengembalian.kt
@@ -1,16 +1,28 @@
 package com.example.perpustakaan.ui.view.pengembalian
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.perpustakaan.R
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
 object DestinasiHomePengembalian : AlamatNavigasi {
     override val route = "homePengembalian"
     override val titleRes = "Data Pengembalian"
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(64.dp),
+        painter = painterResource(R.drawable.iconloading),
+        contentDescription = "Loading"
+    )
 }
 
 @Composable
