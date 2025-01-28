131a15d19ef95c82f5bb56720e9d351286e47631
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 20:20:48 +0700

    Menambahkan DestinasiInsertBuku untuk navigasi entry buku baru

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/buku/InsertBukuView.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.buku
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiInsertBuku: AlamatNavigasi {
+    override val route: String = "item_entry"
+    override val titleRes = "Entry Buku"
+}
