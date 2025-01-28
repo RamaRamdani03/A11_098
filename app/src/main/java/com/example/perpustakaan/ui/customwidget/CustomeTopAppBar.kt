853db67e00416fb0d5815504bc37050265762679
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 07:37:49 +0700

    Menambahkan custom TopAppBar dengan navigasi dan refresh

---
+++ app/src/main/java/com/example/perpustakaan/ui/customwidget/CustomeTopAppBar.kt
@@ -0,0 +1,60 @@
+package com.example.perpustakaan.ui.customwidget
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    showRefresh: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        actions = {
+            if (showRefresh) {
+                IconButton(
+                    onClick = onRefresh,
+                    modifier = Modifier
+                        .padding(end = 16.dp)
+                        .size(36.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Refresh",
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+            }
+        }
+    )
+}
