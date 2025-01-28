d3067a95b83f936d103f8f0cbfa32b7f71fbf434
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 20:58:46 +0700

    Membuat EntryBody untuk form input Buku dengan tombol simpan

--- app/src/main/java/com/example/perpustakaan/ui/view/buku/UpdateBukuView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/buku/UpdateBukuView.kt
@@ -1,10 +1,48 @@
 package com.example.perpustakaan.ui.view.buku
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+import com.example.perpustakaan.ui.viewmodel.buku.InsertBukuUiEvent
+import com.example.perpustakaan.ui.viewmodel.buku.InsertBukuUiState
 
 object DestinasiUpdateBuku: AlamatNavigasi {
     override val route = "update"
     override val titleRes: String = "Update Buku"
     const val id_buku = "id_buku"
     val routesWithArg = "$route/{$id_buku}"
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertBukuUiState,
+    onAnggotaValueChange: (InsertBukuUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormBukuInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onAnggotaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
 }
