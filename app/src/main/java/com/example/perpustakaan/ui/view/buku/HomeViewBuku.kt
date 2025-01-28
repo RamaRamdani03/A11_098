3999e98e4cdf478ce5dd7be4ecda7d52d5560078
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 19:12:26 +0700

    Menambahkan objek DestinasiHomeBuku untuk navigasi ke halaman Menu Buku

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/buku/HomeViewBuku.kt
@@ -0,0 +1,8 @@
+    package com.example.perpustakaan.ui.view.buku
+
+    import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+    object DestinasiHomeBuku : AlamatNavigasi {
+        override val route = "Home Buku"
+        override val titleRes = "Menu Buku"
+    }
