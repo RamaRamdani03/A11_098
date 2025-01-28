273c059f39ccdb62e1834ad5f896fcb3f3c5805d
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 21:52:49 +0700

    Menambahkan objek destinasi navigasi untuk halaman Data Peminjaman

---
+++ app/src/main/java/com/example/perpustakaan/ui/view/peminjaman/HomeViewPeminjaman.kt
@@ -0,0 +1,8 @@
+package com.example.perpustakaan.ui.view.peminjaman
+
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+
+object DestinasiHomePeminjaman : AlamatNavigasi {
+    override val route = "homePeminjaman"
+    override val titleRes = "Data Peminjaman"
+}
