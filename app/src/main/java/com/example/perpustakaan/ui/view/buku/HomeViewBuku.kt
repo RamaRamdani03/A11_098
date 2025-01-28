e504c40b9049041b71f988d52ecfadbd48a9f2d8
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-28 19:42:57 +0700

    Menambahkan Composable functions OnLoading dan OnError untuk tampilan loading dan error dengan tombol retry

--- app/src/main/java/com/example/perpustakaan/ui/view/buku/HomeViewBuku.kt
+++ app/src/main/java/com/example/perpustakaan/ui/view/buku/HomeViewBuku.kt
@@ -1,8 +1,51 @@
-    package com.example.perpustakaan.ui.view.buku
+package com.example.perpustakaan.ui.view.buku
 
-    import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.perpustakaan.R
+import com.example.perpustakaan.ui.navigasi.AlamatNavigasi
 
-    object DestinasiHomeBuku : AlamatNavigasi {
-        override val route = "Home Buku"
-        override val titleRes = "Menu Buku"
+object DestinasiHomeBuku : AlamatNavigasi {
+    override val route = "Home Buku"
+    override val titleRes = "Menu Buku"
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(8.dp),
+        painter = painterResource(R.drawable.iconloading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.erroricon),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
+}
